de3abeb744aed6cd0ad077a783f731a9b5f8aaba
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-03-22 14:48:30 -0400

    abstracted all names

--- routes/main.js
+++ routes/main.js
@@ -4,10 +4,13 @@
  * @description Main routing file.
  */
 
-var pageTitle = 'Rukmal\'s Blog : ';
+var pageTitle = 'Rukmal\'s Blog : '; // This variable controls all titles
+var firstName = 'Rukmal'; // These variables control all page level names
+var lastName = firstName + ' Weerawarana';
 
 exports.index = function(req, res){
     res.render('index',{
-        title: pageTitle + 'Home'
+        title: pageTitle + 'Home',
+        name: firstName
     });
 };
