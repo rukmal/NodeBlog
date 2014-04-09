b270076b0c108079bfa4961bfbe5696325f21819
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-04-09 16:43:35 -0700

    Changed location of disqus script, added actual AppID

--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-	// new Share('.socialbuttons', {
-	// 	ui: {
-	// 		button_background: 'rgb(68,97,159)',
-	// 		button_color: 'rgb(255,255,255)'
-	// 	},
-	// 	networks: {
-	// 		facebook: {
-	// 			app_id: 'abc123'
-	// 		}
-	// 	}
-	// });
+	new Share('.socialbuttons', {
+		ui: {
+			button_background: 'rgb(68,97,159)',
+			button_color: 'rgb(255,255,255)'
+		},
+		networks: {
+			facebook: {
+				app_id: '236305906562640'
+			}
+		}
+	});
 
 	/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
 	var disqus_shortname = 'rukmal-blog'; // required: replace example with your forum shortname
-
 	/* * * DON'T EDIT BELOW THIS LINE * * */
 	(function() {
 		var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-		dsq.src = 'http//' + disqus_shortname + '.disqus.com/embed.js';
+		dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
 		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 	});
+
 });
