70d8042fa4896c94363dc10066481c5b9c86ab6e
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-04-09 15:58:54 -0700

    Removed Facebook SDK

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -6,21 +6,4 @@
 
 $(document).ready(function() {
 	var socket = io.connect('http://localhost');
-
-    $.ajaxSetup({ cache: true });
-
-    $.getScript('http://connect.facebook.net/en_UK/all.js', function() {
-        FB.init({
-            appId: '621121534641370', // <-- YOUR APP ID GOES HERE.
-        });
-        FB.getLoginStatus(function(response) {
-        	var messageReceived = false;
-        	while (!messageReceived) {
-        		socket.emit('facebook-login-info', response);
-        		socket.on('nodeblog-auth-response', function (response) {
-        			messageReceived = response;
-        		});
-        	}
-        });
-    });
 });
--- public/javascripts/post.js
+++ public/javascripts/post.js
@@ -1,12 +1,4 @@
 $(document).ready(function() {
-	// Javascript code for comments (from Facebook SDK)
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=621121534641370";
-		fjs.parentNode.insertBefore(js, fjs);
-	}
 	(document, 'script', 'facebook-jssdk'));
 
 	new Share('.socialbuttons', {
